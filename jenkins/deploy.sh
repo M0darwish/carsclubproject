09f8527b38d8d4356b969cabd16d2159643221ab
M0darwish <mdarwishinbox@gmail.com>
2022-03-16 01:44:26 +0000

    adding database creds to the deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -3,4 +3,7 @@
 echo "Deploy stage"
 
 scp docker-compose.yaml jenkins@Swarm-manager:/home/jenkins/docker-compose.yaml
-ssh jenkins@Swarm-manager docker stack deploy --compose-file docker-compose.yaml carclubapp
+ssh jenkins@Swarm-manager \
+    MYSQL_ROOT_PASSWORD = $MYSQL_ROOT_PASSWORD \
+    SECRET_KEY = $SECRET_KEY \
+    docker stack deploy --compose-file docker-compose.yaml carclubapp
