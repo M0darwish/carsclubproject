2498afab90593eeb919eccef7f2ee4fb7013106b
M0darwish <mdarwishinbox@gmail.com>
2022-03-16 12:42:13 +0000

    building our own nginx image

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -6,4 +6,4 @@ scp docker-compose.yaml jenkins@Swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@Swarm-manager \
     MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
     SECRET_KEY=$SECRET_KEY \
-    docker stack deploy --compose-file docker-compose.yaml carclubapp
+    docker stack deploy --compose-file docker-compose.yaml carclub
