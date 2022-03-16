8507e301e9d6a86da12c0fb0b318a3c2861a31f4
M0darwish <mdarwishinbox@gmail.com>
2022-03-16 02:36:37 +0000

    fixing#solve linking services issue- deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,6 +2,7 @@
 
 echo "Deploy stage"
 
+scp nginx jenkins@Swarm-manager:/home/jenkins/nginx #solve lining the service issue
 scp docker-compose.yaml jenkins@Swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@Swarm-manager \
     MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
