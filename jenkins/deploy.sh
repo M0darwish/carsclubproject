f40d2dc4c2e83102d144b9ce159a7779e4af3586
M0darwish <mdarwishinbox@gmail.com>
2022-03-16 01:04:08 +0000

    adding Deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "Deploy stage"
+
+scp docker-compose.yaml jenkins@Swarm-manager:/home/jenkins/docker-compose.yaml
+ssh jenkins@Swarm-manager docker stack deploy --compose-file docker-compose.yaml carclubapp
