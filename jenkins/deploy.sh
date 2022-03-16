332b11b187685926682104bb08c78353237ae8a9
M0darwish <mdarwishinbox@gmail.com>
2022-03-16 01:49:33 +0000

    fixing database creds in the deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,6 +4,6 @@ echo "Deploy stage"
 
 scp docker-compose.yaml jenkins@Swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@Swarm-manager \
-    MYSQL_ROOT_PASSWORD = $MYSQL_ROOT_PASSWORD \
-    SECRET_KEY = $SECRET_KEY \
+    MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+    SECRET_KEY=$SECRET_KEY \
     docker stack deploy --compose-file docker-compose.yaml carclubapp
