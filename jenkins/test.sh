e3ce1d2df239766931adbea51673d9093e869e5d
M0darwish <mdarwishinbox@gmail.com>
2022-03-16 15:36:09 +0000

    adding unit test to Test stage

---
+++ jenkins/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Test stage"
+
+# Create and activate venv
+python3 -m venv venv
+source venv/bin/activate
+
+#Install pip requirements
+pip3 install -r requirements.txt
+
+# Run pytest
+python3 -m pytest --cov=application
+
+#Remove venv
+deactivate
+rm -rf venv
